176bc4578cca664770ef14c051072eee7f5982ee
Tera Earlywine <dev@teraearlywine.com>
2024-07-24 07:27:02 -0700

    *

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read_requirements():
 
 setup(
     name="cowgirl-ai-core",
-    version="0.0.3",
+    version="0.0.4",
     description="Cowgirl AI - Core Assistant",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
